1b8229093619c05f1ff1493654dcd9c2a1b92bb1
atolmie <atolmie22@gmail.com>
2019-05-30 17:26:55 -0500

    button styles

--- dv-survivors-tool-kit/src/Components/BudgetForm.js
+++ dv-survivors-tool-kit/src/Components/BudgetForm.js
@@ -40,61 +40,65 @@ class BudgetForm extends Component {
 
     return (
       <div>
-      <form className="budgetContainer">
-        <label>
-          <h1>Monthly Budget</h1>
-        </label>
-        <label>
-          <h5>Transportation</h5>
-          <input name="transportation" value={transportation} type="number" onChange={this.handleNumberInput} />
-        </label>
-        <label>
-          <h5>Food</h5>
-          <input name="food" value={food} type="number" onChange={this.handleNumberInput} />
-        </label>
-        <label>
+        <form className="budgetContainer">
           <label>
-            <h5>Health Insurance</h5>
-            <input name="healthinsurance" value={healthinsurance} type="number" onChange={this.handleNumberInput} />
+            <h1>Monthly Budget</h1>
           </label>
           <label>
-            <h5>Car Insurance</h5>
-            <input name="carinsurance" value={carinsurance} type="number" onChange={this.handleNumberInput} />
+            <h5>Transportation</h5>
+            <input name="transportation" value={transportation} type="number" onChange={this.handleNumberInput} />
           </label>
           <label>
-            <h5>Car Loan</h5>
-            <input name="carloan" value={carloan} type="number" onChange={this.handleNumberInput} />
-          </label>
-          <label>
-            <h5>Personal Loan</h5>
-            <input name="personalloan" value={personalloan} type="number" onChange={this.handleNumberInput} />
-          </label>
-          <label>
-            <h5>Cell Phone Bill</h5>
-            <input name="cellphonebill" value={cellphonebill} type="number" onChange={this.handleNumberInput} />
+            <h5>Food</h5>
+            <input name="food" value={food} type="number" onChange={this.handleNumberInput} />
           </label>
           <label>
             <label>
-              <h5>Monthly Rent</h5>
-              <input name="rent" value={rent} type="number" onChange={this.handleNumberInput} />
+              <h5>Health Insurance</h5>
+              <input name="healthinsurance" value={healthinsurance} type="number" onChange={this.handleNumberInput} />
             </label>
             <label>
-              <h5>Utilities </h5>
-              <input name="utilities" value={utilities} type="number" onChange={this.handleNumberInput} />
+              <h5>Car Insurance</h5>
+              <input name="carinsurance" value={carinsurance} type="number" onChange={this.handleNumberInput} />
             </label>
-            <h5>Savings Account</h5>
-            <input name="savings" value={savings} type="number" onChange={this.handleNumberInput} />
+            <label>
+              <h5>Car Loan</h5>
+              <input name="carloan" value={carloan} type="number" onChange={this.handleNumberInput} />
+            </label>
+            <label>
+              <h5>Personal Loan</h5>
+              <input name="personalloan" value={personalloan} type="number" onChange={this.handleNumberInput} />
+            </label>
+            <label>
+              <h5>Cell Phone Bill</h5>
+              <input name="cellphonebill" value={cellphonebill} type="number" onChange={this.handleNumberInput} />
+            </label>
+            <label>
+              <label>
+                <h5>Monthly Rent</h5>
+                <input name="rent" value={rent} type="number" onChange={this.handleNumberInput} />
+              </label>
+              <label>
+                <h5>Utilities </h5>
+                <input name="utilities" value={utilities} type="number" onChange={this.handleNumberInput} />
+              </label>
+              <h5>Savings Account</h5>
+              <input name="savings" value={savings} type="number" onChange={this.handleNumberInput} />
+            </label>
+            <h5>Total</h5>
+            {total}
           </label>
-          <h5>Total</h5>
-          {total}
-        </label>
-      </form>
-      <div>
-      <Link to="/"><button type="button">
-          Next
-     </button> </Link>
+        </form>
+        <div>
+          <Link to="/relocation">
+          <div className="butttonWrapper">
+              <button>
+                Next!
+              </button>
+            </div>
+          </Link>
+        </div>
       </div>
-    </div>
     );
   }
 }
--- dv-survivors-tool-kit/src/Components/RelocationForm.js
+++ dv-survivors-tool-kit/src/Components/RelocationForm.js
@@ -42,74 +42,77 @@ class RelocationForm extends Component {
         const total = hotel + rentaldeposit + firstmonthsrent + utilityconnectionfee + storage + carrental + gasforcar + connectionfee + movingtruck + gasfortruck + mentalhealth + incomeloss + security;
 
         return (
-            
-        <div>
-            <form>
-                <label>
-                    <h1>Relocation Costs</h1>
-                </label>
-                <label>
-                    <h5>Hotel Costs</h5>
-                    <input name="hotel" value={hotel} type="number" onChange={this.handleNumberInput} />
-                </label>
-                <label>
-                    <h5>Rental Deposit</h5>
-                    <input name="rentaldeposit" value={rentaldeposit} type="number" onChange={this.handleNumberInput} />
-                </label>
 
-                <label>
-                    <h5>First Month's Rent</h5>
-                    <input name="firstmonthsrent" value={firstmonthsrent} type="number" onChange={this.handleNumberInput} />
-                </label>
-                <label>
-                    <h5>Utility Connection Fee</h5>
-                    <input name="utilityconnectionfee" value={utilityconnectionfee} type="number" onChange={this.handleNumberInput} />
-                </label>
-                <label>
-                    <h5>Storage Unit</h5>
-                    <input name="storage" value={storage} type="number" onChange={this.handleNumberInput} />
-                </label>
-                <label>
-                    <h5>Car Rental </h5>
-                    <input name="carrental" value={carrental} type="number" onChange={this.handleNumberInput} />
-                </label>
-                <label>
-                    <h5>Gas For Car Rental</h5>
-                    <input name="gasforcar" value={gasforcar} type="number" onChange={this.handleNumberInput} />
-                </label>
-                <label>
-                    <h5>Cell Phone Connection Fee</h5>
-                    <input name="connectionfee" value={connectionfee} type="number" onChange={this.handleNumberInput} />
-                </label>
-                <label>
-                    <h5>Moving Truck Rental</h5>
-                    <input name="movingtruck" value={movingtruck} type="number" onChange={this.handleNumberInput} />
-                </label>
-                <label>
-                    <h5>Gas for Moving Truck </h5>
-                    <input name="gasfortruck" value={gasfortruck} type="number" onChange={this.handleNumberInput} />
-                </label>
-                <label>
-                    <h5>Mental Health Treatment</h5>
-                    <input name="mentalhealth" value={mentalhealth} type="number" onChange={this.handleNumberInput} />
-                </label>
-                <label>
-                    <h5>Income Loss</h5>
-                    <input name="incomeloss" value={incomeloss} type="number" onChange={this.handleNumberInput} />
-                </label>
-                <label>
-                    <h5>Cost of Security Measures</h5>
-                    <input name="security" value={security} type="number" onChange={this.handleNumberInput} />
-                </label>
-                <h5>Total</h5>
-                {total}
-            </form>
             <div>
-            <Link to="/relocation"><button type="button">
-          Next
-     </button></Link>
+                <form>
+                    <label>
+                        <h1>Relocation Costs</h1>
+                    </label>
+                    <label>
+                        <h5>Hotel Costs</h5>
+                        <input name="hotel" value={hotel} type="number" onChange={this.handleNumberInput} />
+                    </label>
+                    <label>
+                        <h5>Rental Deposit</h5>
+                        <input name="rentaldeposit" value={rentaldeposit} type="number" onChange={this.handleNumberInput} />
+                    </label>
+
+                    <label>
+                        <h5>First Month's Rent</h5>
+                        <input name="firstmonthsrent" value={firstmonthsrent} type="number" onChange={this.handleNumberInput} />
+                    </label>
+                    <label>
+                        <h5>Utility Connection Fee</h5>
+                        <input name="utilityconnectionfee" value={utilityconnectionfee} type="number" onChange={this.handleNumberInput} />
+                    </label>
+                    <label>
+                        <h5>Storage Unit</h5>
+                        <input name="storage" value={storage} type="number" onChange={this.handleNumberInput} />
+                    </label>
+                    <label>
+                        <h5>Car Rental </h5>
+                        <input name="carrental" value={carrental} type="number" onChange={this.handleNumberInput} />
+                    </label>
+                    <label>
+                        <h5>Gas For Car Rental</h5>
+                        <input name="gasforcar" value={gasforcar} type="number" onChange={this.handleNumberInput} />
+                    </label>
+                    <label>
+                        <h5>Cell Phone Connection Fee</h5>
+                        <input name="connectionfee" value={connectionfee} type="number" onChange={this.handleNumberInput} />
+                    </label>
+                    <label>
+                        <h5>Moving Truck Rental</h5>
+                        <input name="movingtruck" value={movingtruck} type="number" onChange={this.handleNumberInput} />
+                    </label>
+                    <label>
+                        <h5>Gas for Moving Truck </h5>
+                        <input name="gasfortruck" value={gasfortruck} type="number" onChange={this.handleNumberInput} />
+                    </label>
+                    <label>
+                        <h5>Mental Health Treatment</h5>
+                        <input name="mentalhealth" value={mentalhealth} type="number" onChange={this.handleNumberInput} />
+                    </label>
+                    <label>
+                        <h5>Income Loss</h5>
+                        <input name="incomeloss" value={incomeloss} type="number" onChange={this.handleNumberInput} />
+                    </label>
+                    <label>
+                        <h5>Cost of Security Measures</h5>
+                        <input name="security" value={security} type="number" onChange={this.handleNumberInput} />
+                    </label>
+                    <h5>Total</h5>
+                    {total}
+                </form>
+                <div>
+                    <Link to="/">
+                        <button>
+                            Next!
+                        </button>
+
+                    </Link>
+                </div>
             </div>
-        </div>
         );
     }
 }
