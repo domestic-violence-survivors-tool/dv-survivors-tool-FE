8a80ae591ffc06546583ab9f42cf831e96d5e61c
siratl <elishubax@gmail.com>
2019-05-31 19:55:43 -0700

    Reduced header font size

--- dv-survivors-tool-kit/src/Components/TotalCosts.js
+++ dv-survivors-tool-kit/src/Components/TotalCosts.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
 class TotalCosts extends React.Component {
-    render() {
-        return (
-            <div className="totals">
-                <div>
-                    Your monthly Budget is: ${this.props.budget}
-                </div>
-                <div>
-                    Your total cost for Relocation is: ${this.props.relocation}
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="totals">
+        <div>
+          Your monthly Budget is: <strong>${this.props.budget}</strong>
+        </div>
+        <div>
+          Your total cost for Relocation is:{' '}
+          <strong>${this.props.relocation}</strong>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default TotalCosts;
